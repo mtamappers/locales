e2b596c0f7b392fbdad4c30c4294da759e77de16
arfni <daniel-k@outlook.de>
2024-12-22 13:11:54 +0100

    Added config comment

--- config.ts
+++ config.ts
@@ -1,7 +1,15 @@
 export const i18nConfig = {
   locales: [
-    { name: 'English', code: 'en', iso: 'en-US', alpha2: 'GB-UKM', file: 'en.json', rtl: false },
-    { name: 'German', code: 'de', iso: 'de-DE', alpha2: 'DE', file: 'de.json', rtl: false }
+    /**
+     * name: In english, please.
+     * code: only used internally
+     * tag: BCP 47 language tag, used for date formatting
+     * alpha2: ISO 3166-1 code, used for the flag representation
+     * file: yes
+     * rtl: Does the language use (R)ight (T)o (L)eft text direction?
+     */
+    { name: 'English', code: 'en', tag: 'en-US', alpha2: 'GB-UKM', file: 'en.json', rtl: false },
+    { name: 'German', code: 'de', tag: 'de-DE', alpha2: 'DE', file: 'de.json', rtl: false }
   ],
   defaultLocale: 'en',
   detectBrowserLanguage: {
