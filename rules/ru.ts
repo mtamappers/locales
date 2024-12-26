c3a7a7ef1a6c08711a689d20cb7e0303c18de86f
arfni <daniel-k@outlook.de>
2024-12-26 12:27:09 +0100

    ru: conditional plurazlization

--- rules/ru.ts
+++ rules/ru.ts
@@ -2,14 +2,19 @@ export default function (choice, choicesLength, orgRule) {
   if (choice === 0 || choice === 1) {
     return choice;
   }
-  const teen = choice > 10 && choice < 20
-  const endsWithOne = choice % 10 === 1
-  if (!teen && endsWithOne) {
-    return 1
-  }
-  if (!teen && choice % 10 >= 2 && choice % 10 <= 4) {
-    return 2
-  }
 
-  return choicesLength - 1;
+  if (choicesLength >= 2) {
+    const teen = choice > 10 && choice < 20
+    const endsWithOne = choice % 10 === 1
+    if (!teen && endsWithOne) {
+      return 1
+    }
+    if (!teen && choice % 10 >= 2 && choice % 10 <= 4) {
+      return 2
+    }
+
+    return choicesLength - 1;
+  }
 }
+
+// https://kazupon.github.io/vue-i18n/guide/pluralization.html#custom-pluralization
